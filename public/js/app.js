98e26b33fa9e2f2880befdc831a9e605e3e99438
venkatesh-golla <golla.venkatesh1997@gmail.com>
2020-06-14 20:53:52 -0400

    removed console.log from app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('This is from app.js')
-
 const weatherForm=document.querySelector('form')
 const search=document.querySelector('input')
 const messageOne=document.querySelector('#message-1')
