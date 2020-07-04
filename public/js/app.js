acc4496b1d6294184008eafa66b8e42751fb168a
venkatesh-golla <golla.venkatesh1997@gmail.com>
2020-07-03 22:02:04 -0400

    Button for automatic location detection has been added

--- public/js/app.js
+++ public/js/app.js
@@ -8,7 +8,7 @@ weatherForm.addEventListener('submit',(e)=>{
     const location=search.value
     messageOne.textContent='Loading..'
     messageTwo.textContent=''
-    fetch('/weather?address='+location).then((response)=>{
+    fetch('/weather?address='+location).then((response)=>{ 
 
         response.json().then((data)=>{
             if(data.error){
@@ -20,4 +20,24 @@ weatherForm.addEventListener('submit',(e)=>{
         })
 })
     console.log('Testing')
+})
+
+document.querySelector('#sendLocation').addEventListener('click',(e)=>{
+    if(!navigator.geolocation){
+        document.querySelector('#sendLocation').setAttribute('disabled','disabled')
+        return alert('GeoLocation is not supported by your browser. Please enter your place in the above form')
+    }
+    navigator.geolocation.getCurrentPosition((position)=>{
+        fetch(`/weather?latitude=${position.coords.latitude}&longitude=${position.coords.longitude}`).then((response)=>{ 
+
+            response.json().then((data)=>{
+                if(data.error){
+                    messageOne.textContent=data.error
+                }else{  
+                    messageOne.textContent='Your Location'
+                    messageTwo.textContent=data.data
+                }
+            })
+    })
+    })
 })
